96d9ad1e9a6755f24971d86fa47ba7ab9f058e4d
плотниковс <@403_4_>
2017-07-13 20:24:44 +0600

    Get info about all files in folder

--- FS2/Program.cs
+++ FS2/Program.cs
@@ -18,6 +18,7 @@ namespace FS1
             FileStream fs = file.Create();
             fs.Close();
             ShowWindowsDirectoryInfo();
+            getImageFiles();
         }
 
         static void NewFile()
@@ -29,10 +30,23 @@ namespace FS1
         {
             DirectoryInfo dir = new DirectoryInfo(@"c:\windows");
             Console.WriteLine("****Инфо о файле****");
-            Console.WriteLine("Полный путь: {0}\nНазвание папки: {1}\nРодительский каталог: {2}\nВремя создания: {3}\nАтрибуты: {4}\nКорневой каталог: {5}", dir.FullName, dir.Name, dir.Parent, dir.CreationTime, dir.Attributes, dir.Root);
-            Console.ReadLine();
+            Console.WriteLine("Полный путь: {0}\nНазвание папки: {1}\nРодительский каталог: {2}\n Время создания: {3}\nАтрибуты: {4}\nКорневой каталог: {5}", dir.FullName, dir.Name, dir.Parent, dir.CreationTime, dir.Attributes, dir.Root);
+           // Console.ReadLine();
         }
 
+        static void getImageFiles()
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\плотниковс\Downloads");
+            FileInfo[] imageFiles = dir.GetFiles("*.*", SearchOption.AllDirectories);
+
+            foreach (FileInfo f in imageFiles)
+            {
+                Console.WriteLine("\n**********\n");
+                Console.WriteLine("Имя файла: "+f.Name);
+                Console.WriteLine("Размер файла: "+f.Length);
+                Console.WriteLine("Время создания файла: "+f.CreationTime);
+            }
+        }
 
     }
 }
