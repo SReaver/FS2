a64e6246b2b80e05797da06c07c93cdfb39d44b9
плотниковс <@403_4_>
2017-07-13 19:51:53 +0600

    -

--- FS2/Program.cs
+++ FS2/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace FS2
+namespace FS1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string url = @"C:\Users\плотниковс\Documents\visual studio 2015\Projects\FS2\FS2\text.txt";
+     
+
+            FileInfo file = new FileInfo(url);
+            FileStream fs = file.Create();
+            fs.Close();
+           
         }
+
+        static void NewFile()
+        {
+
+        }
+
+
     }
 }
